9cf944874bf5d8e8fc8eac9278bd92d633ab5858
irfan-vizz <engr.irfanali970@gmail.com>
2025-05-12 10:22:30 +0500

    Remove Prisma

--- app/db.server.js
+++ app/db.server.js
@@ -1,19 +1,19 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 
-if (process.env.NODE_ENV !== "production") {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-}
+// if (process.env.NODE_ENV !== "production") {
+//   if (!global.prisma) {
+//     global.prisma = new PrismaClient();
+//   }
+// }
 
- const prisma = global.prisma || new PrismaClient();
-(async () => {
-  try {
-    const wishlist = await prisma.wishlistConfig.findMany();
-    console.log("Wishlist Config:", wishlist);
-  } catch (error) {
-    console.error("Error fetching wishlist:", error);
-  }
-})();
-//console.log("Wishlist Config:", wishlist);
-export default prisma;
+//  const prisma = global.prisma || new PrismaClient();
+// (async () => {
+//   try {
+//     const wishlist = await prisma.wishlistConfig.findMany();
+//     console.log("Wishlist Config:", wishlist);
+//   } catch (error) {
+//     console.error("Error fetching wishlist:", error);
+//   }
+// })();
+// //console.log("Wishlist Config:", wishlist);
+// export default prisma;
--- prisma/migrations/20250304075332_add_wishlist_config/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "WishlistConfig" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "shop" TEXT NOT NULL,
-    "productIds" TEXT NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "WishlistConfig_shop_key" ON "WishlistConfig"("shop");
