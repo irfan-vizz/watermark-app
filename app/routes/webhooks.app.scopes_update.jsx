914e416c6ad767cdd1f34939fc187ad2056353bd
irfan-vizz <engr.irfanali970@gmail.com>
2025-05-12 10:45:52 +0500

    remove db import

--- app/routes/webhooks.app.scopes_update.jsx
+++ app/routes/webhooks.app.scopes_update.jsx
@@ -1,5 +1,5 @@
 import { authenticate } from "../shopify.server";
-import db from "../db.server";
+// import db from "../db.server";
 
 export const action = async ({ request }) => {
   const { payload, session, topic, shop } = await authenticate.webhook(request);
@@ -8,14 +8,14 @@ export const action = async ({ request }) => {
   const current = payload.current;
 
   if (session) {
-    await db.session.update({
-      where: {
-        id: session.id,
-      },
-      data: {
-        scope: current.toString(),
-      },
-    });
+    // await db.session.update({
+    //   where: {
+    //     id: session.id,
+    //   },
+    //   data: {
+    //     scope: current.toString(),
+    //   },
+    // });
   }
 
   return new Response();
